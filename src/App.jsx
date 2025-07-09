9815f03f0a53023fa2cfd939f5e4c31b48aae7be
Krisztofer Pinkovai <gpusztai94@gmail.com>
2025-07-09 12:57:45 +0300

    Searchbar added

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,30 @@ import './App.css'
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
-  console.log(monsters)
+  const [filteredMonsters, setFilteredMonsters] = useState([]);
+  // console.log(monsters)
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
-    .then((data) => setMonsters(data));
+    .then((data) => {
+      setMonsters(data);
+      setFilteredMonsters(data);
+    });
     }, [])
 
   return (
       <>
-        <CardList monsters={monsters} />
+      <input 
+      type='search' 
+      onChange={(event) => {
+        const searchString = event.target.value.toLowerCase();
+        const newFilteredMonsters = monsters.filter((monster) => 
+          monster.name.toLowerCase().includes(searchString));
+        setFilteredMonsters(newFilteredMonsters);
+        }}
+        />
+        <CardList monsters={filteredMonsters} />
       </>
   );
 };
